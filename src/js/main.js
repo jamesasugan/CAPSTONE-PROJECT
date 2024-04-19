6b962e024d7d1e8abb514b9aceb8a2dcc3790880
jamesasugan <jjasugan@gmail.com>
2024-04-20 01:21:31 +0800

    updated

--- src/js/main.js
+++ src/js/main.js
@@ -113,16 +113,3 @@ document
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('.theme-controller').checked = false;
 });
-
-// for eye password icon
-function togglePasswordVisibility() {
-  var passwordInput = document.getElementById('password');
-  var passwordIcon = document.getElementById('password-icon');
-  if (passwordInput.type === 'password') {
-    passwordInput.type = 'text';
-    passwordIcon.className = 'fas fa-eye-slash';
-  } else {
-    passwordInput.type = 'password';
-    passwordIcon.className = 'fas fa-eye';
-  }
-}
